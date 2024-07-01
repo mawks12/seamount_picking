545e305edb8fea785673578358957ff668f5b871
Martin <martinhawks8@gmail.com>
2024-07-01 12:24:53 -0700

    fixed vscode merge issues

--- code/smount_predictors/src/SeamountHelp.py
+++ code/smount_predictors/src/SeamountHelp.py
@@ -5,25 +5,13 @@ File handling optimization and other basic task
 specific functions
 """
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 from pathlib import Path
 import numpy as np
 import plotly.express as px
 from plotly.graph_objs._figure import Figure
-from depreciated.DBSCANModel import DBSCANModel
-# from sklearn.cluster import HDBSCAN
-import pandas as pd
-=======
 import numpy as np
 import plotly.express as px
 from plotly.graph_objs._figure import Figure
->>>>>>> 0524657 (Added seamount ml classes to interface with sklearn libraries)
-=======
-import numpy as np
-import plotly.express as px
-from plotly.graph_objs._figure import Figure
->>>>>>> 0524657 (Added seamount ml classes to interface with sklearn libraries)
 
 def readCroppedxyz(io,  bounds: tuple[float, float, float, float]) -> np.ndarray:
     """
@@ -50,8 +38,6 @@ def readCroppedxyz(io,  bounds: tuple[float, float, float, float]) -> np.ndarray
     out = []
     for line in io:
         line = line.split()
-<<<<<<< HEAD
-<<<<<<< HEAD
         if bounds[2] <= float(line[0]) and bounds[3] >= float(line[0]):  # Check Lon bounds
             if bounds[0] <= float(line[1]) and bounds[1] >= float(line[1]):  # Check Lat bounds
                 out.append([float(i) for i in line])
@@ -80,22 +66,7 @@ def filterData(data: np.ndarray, bounds: tuple[float, float, float, float]) -> n
     data = data[data[:, 1] <= bounds[3]]
     return data
 
-def plotData(data: pd.DataFrame, colarval="Intensity", op=1.0) -> Figure:
-=======
-=======
->>>>>>> 0524657 (Added seamount ml classes to interface with sklearn libraries)
-        if bounds[2] <= float(line[0]) and bounds[3] >= float(line[0]):  # Check Lat bounds
-            if bounds[0] <= float(line[1]) and bounds[1] >= float(line[1]):  # Check Lon bounds
-                out.append([float(i) for i in line])
-        else:
-            continue
-    return np.array(out)
-
-def plotData(data, colarval="Intensity") -> Figure:
-<<<<<<< HEAD
->>>>>>> 0524657 (Added seamount ml classes to interface with sklearn libraries)
-=======
->>>>>>> 0524657 (Added seamount ml classes to interface with sklearn libraries)
+def plotData(data, colarval="Intensity", op=1) -> Figure:
     """
     generates a plot of Lat Lon Intensity data
     Parameters
@@ -107,67 +78,13 @@ def plotData(data, colarval="Intensity") -> Figure:
     fig: Figure
         plot of data
     """
-<<<<<<< HEAD
-<<<<<<< HEAD
     fig = px.scatter(data, x="Longitude", y="Latitude", color=colarval, opacity=op)
-=======
     fig = px.scatter(data, x="Longitude", y="Latitude", color=colarval)
->>>>>>> 0524657 (Added seamount ml classes to interface with sklearn libraries)
-=======
     fig = px.scatter(data, x="Longitude", y="Latitude", color=colarval)
->>>>>>> 0524657 (Added seamount ml classes to interface with sklearn libraries)
     fig.update_xaxes(
     scaleanchor="y",
     scaleratio=1,
   )
-    return fig
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-def testNewZone(bounds, data, params=(0.32052631578947366, 13)):
-    """
-    Tests a new zone for seamounts using the DBSCAN algorithm
-    
-    Parameters
-    ----------
-    bounds: tuple
-        area being filtered for of the form (minlat, maxlat, minlon, maxlon)
-    data: np.ndarray
-        data to be tested
-    params: tuple
-        parameters for the DBSCAN algorithm, defaults to best found params
-        found in the DBSCAN training notebook
-    Returns
-    -------
-    labels: pd.DataFrame
-        set of labeled data points
-    """
-    data = filterData(data, bounds)
-    model = DBSCANModel(data, params)
-    labels = model.getClusters()
-    #score = model.scoreTestData(data)
-    return labels
-
-def plotTestZone(bounds, data, params=(0.32052631578947366, 13)):
-    """
-    Tests a new zone for seamounts using the DBSCAN algorithm
-    and plots the results
-    
-    Parameters
-    ----------
-    bounds: tuple
-        area being filtered for of the form (minlat, maxlat, minlon, maxlon)
-    data: np.ndarray
-        data to be tested
-    params: tuple
-        parameters for the DBSCAN algorithm, defaults to best found params
-        found in the DBSCAN training notebook
-    """
-    data = filterData(data, bounds)
-    model = DBSCANModel(data, params)
-    labels = model.getClusters()
-    labels = labels[labels["Cluster"] != labels["Cluster"].mode()[0]]
-    fig = plotData(labels, "TrueVal")
     return fig
 
 # def divClusters(data: np.ndarray):
@@ -211,7 +128,3 @@ def exportCentroids(centers, filename=Path('data') / 'centered_all.xy', mountnam
     with open(filename, 'w', encoding='utf-8') as f:
         for ind, val in enumerate(centers):
             f.write(f"{mountname}-{ind} {val[1]} {val[0]}\n")
-=======
->>>>>>> 0524657 (Added seamount ml classes to interface with sklearn libraries)
-=======
->>>>>>> 0524657 (Added seamount ml classes to interface with sklearn libraries)
